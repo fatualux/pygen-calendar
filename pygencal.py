2c0c92dc07bf4dbb72fbcc2f05aea67eaec5399c
fatualux <35587292+fatualux@users.noreply.github.com>
2024-05-22 17:59:54 +0200

    Put weeks in columns

---
+++ pygencal.py
@@ -0,0 +1,89 @@
+from flask import Flask, render_template, request
+import calendar
+import os
+from utils import linkify, add_notes, generate_css, italian_weekday_abbr
+import webbrowser
+
+app = Flask(__name__)
+
+
+# Function to open browser on server startup
+def startup_server(address, port):
+    if os.name == 'nt':
+        address = '127.0.0.1'
+    webbrowser.open(f"http://{address}:{port}")
+
+startup_server('0.0.0.0', 5000)
+
+# Define Italian month names
+italian_month_names = ['', 'Gennaio', 'Febbraio', 'Marzo', 'Aprile',
+                        'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre',
+                       'Ottobre', 'Novembre', 'Dicembre']
+
+# Create HTML folder if it doesn't exist
+html_folder = 'HTML'
+if not os.path.exists(html_folder):
+    os.makedirs(html_folder)
+
+# Route to handle form submission and display result
+@app.route('/generate_calendar', methods=['POST'])
+def generate_calendar():
+    # Get form data
+    year = int(request.form['year'])
+    month = int(request.form['month'])
+    font_name = request.form['font_name']
+
+    # Generate CSS file with the user-provided font name
+    generate_css(font_name)
+
+    # Create month calendar and add notes
+    month_calendar = {day: [] for day in range(1, calendar.monthrange(year, month)[1] + 1)}
+    month_calendar = add_notes(month_calendar)
+
+    # Generate HTML content
+    output = f'<!DOCTYPE html>\n<html>\n<head>\n<title>{italian_month_names[month]} {year}</title>\n'
+    output += f'<link rel="stylesheet" type="text/css" href="calendar_style.css">\n</head>\n<body>\n'
+
+    # Generate table with weeks as columns
+    output += f'<table border="1">\n<tr><th colspan="{len(calendar.monthcalendar(year, month)) + 1}"><h1>{italian_month_names[month]} {year}</h1></th></tr>\n'
+
+    raw_calendar = calendar.monthcalendar(year, month)
+    max_weeks = len(raw_calendar)
+
+    # Transpose the raw_calendar to have weeks as columns
+    for day_idx in range(7):
+        output += '<tr>'
+        for week_idx in range(max_weeks):
+            day = raw_calendar[week_idx][day_idx]
+            if day != 0:
+                # Get the Italian weekday abbreviation
+                weekday = italian_weekday_abbr[calendar.weekday(year, month, day)]
+                # Check if there are notes for this day
+                notes = '<br>'.join(['<span style="color: {}; font-weight: bold;">{}</span>'.format(color, note.replace("\\n", "<br>")) for note, color in month_calendar[day]])
+                # Add styling to create a frame around the cells with notes
+                if month_calendar[day]:
+                    output += f'<td style="border-left: 3px solid {month_calendar[day][0][1]}; border-top: 3px solid {month_calendar[day][0][1]}; border-bottom: 3px solid {month_calendar[day][0][1]};">{weekday}</td><td style="border-top: 3px solid {month_calendar[day][0][1]}; border-bottom: 3px solid {month_calendar[day][0][1]};">{day}</td><td style="border-top: 3px solid {month_calendar[day][0][1]}; border-bottom: 3px solid {month_calendar[day][0][1]}; border-right: 3px solid {month_calendar[day][0][1]};">{notes}</td>'
+                else:
+                    output += f'<td>{weekday}</td><td>{day}</td><td>{notes}</td>'
+            else:
+                output += '<td></td><td></td><td></td>'
+        output += '</tr>\n'
+
+    output += '</table>\n</body>\n</html>'
+
+    # Save HTML file
+    filename = os.path.join(html_folder, f"{year}-{month}.html")
+    with open(filename, 'w') as f:
+        f.write(output)
+
+    # Render template with generated calendar content
+    return render_template('pygencal_result.html', calendar_html=output)
+
+
+@app.route('/')
+def index():
+    return render_template('pygencal_index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ venv.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Read configuration from venv.cfg file
+source ./venv.cfg
+
+deactivate_venv() {
+  echo "Deactivating virtual environment..."
+  deactivate
+  echo "Virtual environment deactivated."
+}
+
+# Trap Ctrl+C and call deactivate_venv function
+trap deactivate_venv SIGINT
+
+dependency_check() {
+  if command -v $VENV_COMMAND &> /dev/null; then
+    echo "vOk, virtualenv package is installed on your system."
+  else
+    echo "A suitable Python virtual environment is not installed on your system."
+    echo "Please install virtualenv and try again."
+    echo "Aborting..."
+    exit
+  fi
+}
+
+venv_create() {
+  # Remove venv directory if it exists
+  if [ -d "$WORKDIR" ]; then
+    rm -rf $WORKDIR
+    echo "Old virtual environment deleted."
+  fi
+  echo "Starting Python Virtual Environment setup..."
+  echo ""
+  echo "Python Virtual Environment not found. Creating..."
+  $VENV_COMMAND $WORKDIR
+  echo ""
+  echo "Python virtual environment created."
+  echo ""
+}
+
+copy_files() {
+  echo "Copying requirements..."
+  cp $REQUIREMENTS_FILE $WORKDIR
+  echo "Copying application file..."
+  cp $APP_FILE $WORKDIR
+  echo "Copying static files..."
+  cp -r $STATIC_FILE $WORKDIR
+  echo "Copying template files..."
+  cp -r $TEMPLATE_FILE $WORKDIR
+  echo "Copying modules..."
+    cp -r $UTILS_FILE $WORKDIR
+  echo ""
+  echo "Done."
+}
+
+venv_activate() {
+  echo "Activating virtual environment..."
+  source $WORKDIR/bin/activate
+  echo ""
+}
+
+install_req() {
+  echo "Installing requirements..."
+  pip install --upgrade pip && pip install -r $REQUIREMENTS_FILE
+  echo ""
+  echo "Done."
+}
+
+starting_app() {
+  echo "Starting application..."
+  echo ""
+  cd $WORKDIR && python $APP_FILE
+}
+
+dependency_check
+venv_create
+copy_files
+venv_activate
+install_req
+starting_app
